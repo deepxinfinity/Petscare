1ab034522316506b9f83a8ba12825fee51e1784a
deepxinfinity <deepankshokeen@gmail.com>
2023-06-25 16:48:19 +0530

    Support pages updated

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -7,25 +7,24 @@ const Contact = () => {
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
+            src="/images/contact.jpg"
+            alt="contact"
             style={{ width: "100%" }}
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-6" >
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7
-            vaialible
+          At Petcare, we value your satisfaction and aim to provide exceptional customer service. Whether you have questions about our products, require guidance on pet care, or need assistance with an order, our dedicated team is here to assist you.
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : support@petscare.com
           </p>
           <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
+            <BiPhoneCall /> : 99999999
           </p>
           <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport /> : 1800-0000-0000 
           </p>
         </div>
       </div>
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,24 +4,21 @@ import Layout from "./../components/Layout/Layout";
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
-      <div className="row contactus ">
+      <div className="row privacy ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
+            src="/images/contact.jpg"
+            alt="contact"
             style={{ width: "100%" }}
           />
         </div>
-        <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+        <div className="col-md-6">
+          <p className="text-justify mt-2">
+          We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site, place an order, subscribe to the newsletter, respond to a survey, fill out a form, and in connection with other activities, services, features, or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, and credit card information. Users may, however, visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain Site-related activities          </p>
+          By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.
         </div>
       </div>
+      
     </Layout>
   );
 };
